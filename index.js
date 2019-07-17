2944ce1156007e4526047370d9d8ca1a0957c22c
Gael Leblan <gael.leblan@yahoo.fr>
2019-07-17 11:45:43 +0200

    Ajout page Sonar

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const fs = require('fs-extra');
+const util = require('util');
+const moment = require('moment');
+const phantom = require('phantom');
+const path = require('path');
+const app = express();
+var instance;
+var page;
 var config = require('./config.json');
 var sentences = require('./data/sentences.json');
 var rotation = require('./rotation');
 var holiday = require('./holiday');
+
+var dir = path.join(__dirname, 'images');
+
+app.use(express.static(dir));
 
 //Parser for the body of request
 app.use(bodyParser.urlencoded({
@@ -32,15 +42,15 @@ app.post('/shiftrotation', function(req, res) {
   let direction = req.param('direction', 1);
   rotation.shiftRotaation(direction);
   res.send(200);
-}); 
+});
 
 app.get('/rotation', function(req, res) {
   res.json(rotation.getRotation(1));
-}); 
+});
 
 app.get('/rotationMonth', function(req, res) {
   res.json(rotation.getRotation(4));
-}); 
+});
 
 app.get('/sentences', function(req, res) {
   res.json(sentences);
@@ -48,7 +58,7 @@ app.get('/sentences', function(req, res) {
 
 app.get('/holidays', function(req, res) {
   res.json(holiday.getHolidays());
-}); 
+});
 
 app.put('/holidays', function(req, res) {
   let data = req.body;
@@ -86,6 +96,68 @@ app.options("/*", function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   res.send(200);
 });
+
+setInterval(() => {
+  let now = moment();
+  let yesterday = moment().subtract(1, 'day');
+  if (now.hour() > 8) {
+    checkImageExist(now);
+
+    clearImages();
+  }
+}, 10000);
+
+(async () => {
+  //setup PhantomJS
+instance = await phantom.create();
+page = await instance.createPage();
+
+//Set the viewPort size
+await page.property('viewportSize', { width: 1280, height: 1400 });
+await page.property('clipRect', { top: 130, left: 18, width: 950, height: 1200 });
+page.on('onResourceError',(resourceError) => {
+  console.error(resourceError.url + ': ' + resourceError.errorString);
+});
+})();
+
+var renderImage = async(url, fileName, page) => {
+  //Open the page
+  const status = await page.open(url);
+  console.log(`Page opened with status [${status}].`);
+
+  if(status === 'success') {
+    //Let a bit of time for page to render
+    setTimeout(async () => {
+      await page.render(fileName);
+    }, 10000);
+  }
+};
+
+var clearImages = function() {
+  let directory = './images';
+  fs.readdir(directory, (err, files) => {
+    files.forEach(file => {
+      let date = file.substring(0, file.indexOf('.'));
+      let dateMoment = moment(date);
+      if(dateMoment.isBefore(moment().subtract(5, 'day'))) {
+        fs.unlink(directory + '/' + file);
+      }
+    });
+  });
+}
+
+var checkImageExist = (time) => {
+  let imageName = './images/' + time.format('YYYY-MM-DD') + '.jpg';
+    try {
+      if (fs.existsSync(imageName)) {
+        //file exists, everything is fine
+      } else {
+        renderImage('http://sonarqube-prd.intra.arkea.com:8080/portfolio?id=BAD_Creature_Java1',imageName, page);
+      }
+    } catch (err) {
+      console.error(err)
+    }
+}
 
 //Launch the server
 app.listen(3000, function() {
